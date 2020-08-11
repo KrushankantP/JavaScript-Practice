500fda1e576a803b651e0cc2b4cba5a69823a14c
Patel Krushankant <patelkk1618@gmail.com>
2020-08-11 02:02:16 -0500

    Lesson 5 Functions

---
+++ IntroTo Javascript/5. Functions/Quize or challenges/5. cry (function Expression).js
@@ -0,0 +1,19 @@
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should have a variable `cry`
+ * - Your code should include a named function expression stored in the variable `cry`
+ * - Your code should call the function expression using the variable name, not the function name
+ * - Your function expression should return the expected output
+ * - BE CAREFUL ABOUT THE PUNCTUATION AND THE EXACT WORDS TO BE PRINTED.
+ */
+
+
+// your code goes here
+
+var cry = function myFunction(){
+    var sound = "boohoo!";
+    return sound;
+};
+
+// Call the function using the variable name, not the function name
+console.log(cry());
